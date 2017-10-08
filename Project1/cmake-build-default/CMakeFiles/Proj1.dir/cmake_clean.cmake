d2ebae1bf335239348e5c7bd0f88db147ec02b90
deonj197 <deonj197@gmail.com>
2017-10-07 22:17:18 -0400

    Initial Commit & Uploading Project 1

---
+++ Project1/cmake-build-default/CMakeFiles/Proj1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proj1.dir/main.cpp.obj"
+  "Proj1.pdb"
+  "Proj1.exe"
+  "Proj1.exe.manifest"
+  "libProj1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proj1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
