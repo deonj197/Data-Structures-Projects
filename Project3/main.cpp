00b1e98843181fb571caabd49d0aa9f4ddf62735
deonj197 <deonj197@gmail.com>
2017-11-13 17:21:05 -0500

    Adding Comments and cleaning some things.

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -12,14 +12,16 @@ using std::cout; using std::cin; using std::endl;
 
 #include <cmath>
 
+#include <limits.h>
+
 #include <string>
 
 using std::string;
 
 struct Loon {
-    int xCord;
-    int yCord;
-    bool visited;
+    double xCord;
+    double yCord;
+    bool visited = false;
     bool startingPos = false;
 
     Loon();
@@ -38,14 +40,14 @@ public:
 
     void addLoon(Loon *loon);
 
-    void printGraph();
-
     string determineIfAllCanBeReached();
 
 };
 
 Loon::Loon() {
-
+    // Initialize to the upper bounds of x and y.
+    this->xCord = INFINITY;
+    this->yCord = 20.0;
 }
 
 Loon::Loon(int xCord, int yCord) {
@@ -62,20 +64,24 @@ void Graph::addLoon(Loon *loon) {
 }
 
 void Graph::visitClosestTwoStations(Loon *loon) {
-    Loon *otherLoon1 = new Loon(100,100);
-    Loon *otherLoon2 = new Loon(100,100);
-    double minDistance = 100;
+    auto *otherLoon1 = new Loon;
+    auto *otherLoon2 = new Loon;
+    double minDistance = INFINITY;
     double distToNextLoon;
 
     for (auto nextLoon : loons) {
+        // Make sure the next balloon in the graph is not the one we are finding the closest two for.
         if (nextLoon->xCord != loon->xCord || nextLoon->yCord != loon->yCord) {
+            // Apply the linear distance formula to find distance between balloons.
             distToNextLoon = sqrt(
                     pow((nextLoon->xCord - loon->xCord), 2) +
                     pow((nextLoon->yCord - loon->yCord), 2)
             );
+            // Reassign min if appropriate.
             if (distToNextLoon < minDistance) {
                 minDistance = distToNextLoon;
                 otherLoon1 = nextLoon;
+            // Handle the ties.
             } else if (distToNextLoon == minDistance) {
                 if (nextLoon->xCord < otherLoon1->xCord) {
                     otherLoon1 = nextLoon;
@@ -87,17 +93,24 @@ void Graph::visitClosestTwoStations(Loon *loon) {
             }
         }
     }
-    minDistance = 100;
+
+    // Find the second place winner (Second closest balloon)
+    minDistance = INFINITY;
     for (auto nextLoon : loons) {
+        // Make sure the next balloon in the graph is not the one we are finding the closest two for.
         if ((nextLoon->xCord != loon->xCord || nextLoon->yCord != loon->yCord) &&
+                // Exclude the balloon that one first place already.
                 (nextLoon->xCord != otherLoon1->xCord || nextLoon->yCord != otherLoon1->yCord)) {
+            // Apply the linear distance formula to find distance between balloons.
             distToNextLoon = sqrt(
                     pow((nextLoon->xCord - loon->xCord), 2) +
                     pow((nextLoon->yCord - loon->yCord), 2)
             );
+            // Reassign min if appropriate.
             if (distToNextLoon < minDistance) {
                 minDistance = distToNextLoon;
                 otherLoon2 = nextLoon;
+            // Handle the ties.
             } else if (distToNextLoon == minDistance) {
                 if (nextLoon->xCord < otherLoon2->xCord) {
                     otherLoon2 = nextLoon;
@@ -109,12 +122,16 @@ void Graph::visitClosestTwoStations(Loon *loon) {
             }
         }
     }
+    // Check base case which is both the closest two have already been visited.
+    // Call this method recursively on the two closest balloons until base case is met.
     if (!otherLoon1->visited) {
+        // Flag the first place winner as visited.
         otherLoon1->visited = true;
         visitClosestTwoStations(otherLoon1);
     }
 
     if (!otherLoon2->visited) {
+        // Flag the second place winner as visited.
         otherLoon2->visited = true;
         visitClosestTwoStations(otherLoon2);
     }
@@ -135,12 +152,6 @@ string Graph::determineIfAllCanBeReached() {
     }
 
     return "yes";
-}
-
-void Graph::printGraph() {
-    for (auto loon : loons) {
-        cout << loon->visited << endl;
-    }
 }
 
 int main() {
